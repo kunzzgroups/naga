7eff7b1c1c13a4f894e579e6cc086918a849825a
Wang Zai <paxit01@gmail.com>
2026-06-15 11:05:31 +0800

    Dev to Prod

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,8 +2,8 @@
 // Update API base URL here only. Other JS files can read from window.NAGA_CONFIG and window.NAGA_API.
 window.NAGA_CONFIG = {
   api: {
-    baseUrl: 'http://localhost:8080',
-    // baseUrl: 'https://bo.corepayx.com'
+    // baseUrl: 'http://localhost:8080',
+    baseUrl: 'https://bo.corepayx.com'
   }
 };
 
